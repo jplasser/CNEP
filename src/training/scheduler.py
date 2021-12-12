c177eef44a2cf9ffb0c10dccc42f308f1e7b9b6c
Jürgen Plasser <juergen@plasser.net>
2021-12-12 13:58:11 +0100

    take resume with the same LR schedule into consideration (experimental)

--- src/training/scheduler.py
+++ src/training/scheduler.py
@@ -7,13 +7,13 @@ def assign_learning_rate(optimizer, new_lr):
 def _warmup_lr(base_lr, warmup_length, step):
     return base_lr * (step + 1) / warmup_length
 
-def cosine_lr(optimizer, base_lr, warmup_length, steps):
+def cosine_lr(optimizer, base_lr, warmup_length, steps, start_step=0):
     def _lr_adjuster(step):
-        if step < warmup_length:
-            lr = _warmup_lr(base_lr, warmup_length, step)
+        if (step - start_step) < warmup_length:
+            lr = _warmup_lr(base_lr, warmup_length, (step - start_step))
         else:
-            e = step - warmup_length
-            es = steps - warmup_length
+            e = (step - start_step) - warmup_length
+            es = (steps - start_step) - warmup_length
             lr = 0.5 * (1 + np.cos(np.pi * e / es)) * base_lr
         assign_learning_rate(optimizer, lr)
         return lr
